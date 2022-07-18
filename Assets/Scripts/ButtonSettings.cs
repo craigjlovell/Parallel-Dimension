8b860ab2a9da8b4979a9ebd53c9a09cc61e16383
craig <craig@DESKTOP-BJL3O96>
2022-07-19 00:57:50 +1000

    says

--- Assets/Scripts/ButtonSettings.cs
+++ Assets/Scripts/ButtonSettings.cs
@@ -3,10 +3,26 @@ using System;
 using System.Collections;
 
 [Serializable]
-public class ButtonSettings
+public class ButtonSettings : MonoBehaviour
 {
-
     public Color normalColor;
     public Color highlightColor;
 
+    float resetDelay = .25f;
+
+    private void Start()
+    {
+        ResetButton();
+    }
+
+    public void OnMouseDown()
+    {
+        GetComponent<MeshRenderer>().material.color = highlightColor;
+        Invoke("ResetButton", resetDelay);
+    }
+
+    public void ResetButton()
+    {
+        GetComponent<MeshRenderer>().material.color = normalColor;
+    }
 }
--- Assets/Scripts/SimonSays.cs
+++ Assets/Scripts/SimonSays.cs
@@ -5,12 +5,7 @@ using UnityEngine.UI;
 
 public class SimonSays : MonoBehaviour
 {
-    public List<GameObject> gameButtonPrefab;
-    public List<GameObject> gameButtons;
-
-    public Transform gameFieldPanelTransform;
-
-    public List<ButtonSettings> buttonSettings;
+    public ButtonSettings[] gameButtons;
 
     public float[] seq1 = new float[1] { 2 };
     public float[] seq2 = new float[2] { 2, 0 };
@@ -19,52 +14,15 @@ public class SimonSays : MonoBehaviour
 
     public float[] safeCode = { 2, 0, 1, 3 };
 
-    bool inputEnabled = false;
-    bool gameOver = false;
-
-    System.Random rg;
-
-    int bleepCount = 3;
-
-    List<int> bleeps;
-    List<int> playerBleeps;
-
     // Start is called before the first frame update
     void Start()
     {
-        gameButtons = new List<GameObject>();
+        gameButtons[4] = new ButtonSettings();
 
-        ButtonCreating(0, new Vector3(-0.1045986f, 0.25f, 1.511185f));
-        ButtonCreating(1, new Vector3(0.3954014f, 0.25f, 1.511185f));
-        ButtonCreating(2, new Vector3(-0.1045986f, -0.25f, 1.511185f));
-        ButtonCreating(3, new Vector3(0.3954014f, -0.25f, 1.511185f));
-
-        StartCoroutine(SimonSaysEnum());
-    }
-
-    IEnumerator SimonSaysEnum()
-    {
-        inputEnabled = false;
-
-        rg = new System.Random("Test".GetHashCode());
-
-        SetBleeps();
-
-
-        yield return null;
-    }
-
-    void SetBleeps()
-    {
-        bleeps = new List<int>();
-        playerBleeps = new List<int>();
-
-        for (int i = 0; i < bleepCount; i++)
+        for (int i = 0; i < gameButtons.Length; i++)
         {
-            bleeps.Add(rg.Next(0, gameButtons.Count));
-        }
 
-        bleepCount++;
+        }
     }
 
     // Update is called once per frame
@@ -75,30 +33,14 @@ public class SimonSays : MonoBehaviour
 
     public void ButtonCreating(int index, Vector3 position)
     {
-        foreach (GameObject game in gameButtonPrefab)
-        {
-            game.GetComponent<Button>().onClick.AddListener(() =>
-            {
-                SaysButtonPressed(index);
-            });
-
-            gameButtons.Add(game);
-        }
-
 
     }
 
     public void SaysButtonPressed(int index)
     {
-        if (!inputEnabled)
-            return;
-
-        // play button glow if pressed todo
     }
 
     public void GameOver()
     {
-        gameOver = true;
-        inputEnabled = false;
     }
 }
