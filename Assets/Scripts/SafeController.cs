12c4316527bba52a977e1b823f2fade32f52111e
Jazz <jaspereyers@hotmail.com>
2022-07-19 15:38:52 +1000

    safe, simon says and main menu set up 3:38 19/07/22

--- Assets/Scripts/SafeController.cs
+++ Assets/Scripts/SafeController.cs
@@ -6,11 +6,15 @@ public class SafeController : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI text;
 
-    public int ButtonValue0;
-    public int ButtonValue1;
-    public int ButtonValue2;
-    public int ButtonValue3;
-    
+    public int buttonValue0;
+    public int buttonValue1;
+    public int buttonValue2;
+    public int buttonValue3;
+    public int[] simonCode = new int[4];
+    public int[] codeCypher = new int[4];
+    public SimonSays2 simonSays;
+    public int index = 0;
+    public int trueCheck;
 
 
     public void InputButton(int PIN)
@@ -18,16 +22,60 @@ public class SafeController : MonoBehaviour
         if (text.text.Length < 4)
         {
             text.text += PIN;
+            simonCode[index] = PIN;
+            index++;
         }
+
     }
 
     public void Backspace()
     {
         text.text = "";
+        index = 0;
     }
 
     public void Enter()
     {
+        for (int i = 0; i < codeCypher.Length; i++)
+        {
+            if (simonSays.Sequence[i] == 0)
+            {
+                codeCypher[i] = buttonValue0;
+            }
+            else if (simonSays.Sequence[i] == 1)
+            {
+                codeCypher[i] = buttonValue1;
+            }
+            else if (simonSays.Sequence[0] == 2)
+            {
+                codeCypher[i] = buttonValue2;
+            }
+            else if (simonSays.Sequence[1] == 3)
+            {
+                codeCypher[i] = buttonValue3;
+            }
+        }
 
+        trueCheck = 0;
+        for(int i = 0; i < simonCode.Length-1; i++)
+        {
+            if (!simonCode[i].Equals(simonSays.Sequence[i]))
+            {
+                Debug.Log("lose");
+            }
+            else
+            {
+                trueCheck++;
+            }
+        }
+        if (trueCheck == 3)
+        {
+            Debug.Log("win");
+            text.text = "";
+        }
+        else
+        {
+            trueCheck = 0;
+        }
     }
 }
